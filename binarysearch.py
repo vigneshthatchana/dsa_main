c6a632ac1e1ca4f3c88b152d124f10db0b5e4688
vigneshthatchana <tsvigneshthatchana@gmail.com>
2026-03-11 20:10:13 +0530

    binarysearch.py file is created

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+def binarysearch(arr,target):
+    left = 0
+    right = len(arr)-1
+
+    while left <= right:
+        mid = (left+right) //2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid + 1
+    return -1
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+target = 9
+
+print(binarysearch(arr,target))
