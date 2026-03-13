9e423bc8ce68c7067d1c6fcb75ed2fd6b715c021
vigneshthatchana <tsvigneshthatchana@gmail.com>
2026-03-13 20:31:47 +0530

    updated prefixsum.py file

--- Array/prefixsum.py
+++ Array/prefixsum.py
@@ -1,1 +1,9 @@
+arr = [92,79,93,97,94]
 
+prefix_sum = []
+current_sum = 0
+
+for i in range(len(arr)):
+    current_sum = arr[i] + current_sum
+    prefix_sum.append(current_sum)
+print(prefix_sum)
