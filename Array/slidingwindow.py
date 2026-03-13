833d391c7c2a49b63fbc44e551d4cac50b859770
vigneshthatchana <70791244+vigneshthatchana@users.noreply.github.com>
2026-03-13 21:54:49 +0530

    Update slidingwindow.py

--- Array/slidingwindow.py
+++ Array/slidingwindow.py
@@ -1,4 +1,4 @@
-arr = [92,79,93,97,94]
+arr = [1,2,3,4,5,6,7,8]
 
 k = 3
 
@@ -6,3 +6,6 @@ window_sum = sum(arr[0:3])
 max_sum = window_sum
 
 for i in range(k,len(arr):
+  window_sum = window_sum - arr[i-k] + arr[i]
+  max_sum = max_sum(window_sum,max_sum)
+print(max_sum)
